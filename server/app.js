6bb6c2f217ae1fa61bf2b947f49b6d463cca30f0
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2016-07-22 23:25:55 -0500

    chat feature works

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 var express = require('express');
+var app = express();
+var socketio  = require('socket.io');
 var path = require('path');
 var bodyParser = require('body-parser');
-io = require('socket.io').listen(app),
-fs = require('fs');
-var app = express();
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
@@ -13,8 +13,29 @@ app.get('/', function(req, res){
   res.sendFile(path.resolve('views/index.html'));
 });
 
-app.listen(3000, function(req, res){
+var server = app.listen(3000, function(req, res){
   console.log("server is now listening on port 3000");
+});
+
+var io = require('socket.io').listen(server);
+
+
+
+io.sockets.on('connection', function (socket) {
+console.log('a user is connected');
+
+socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+
+io.sockets.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+
+  });
 });
 
 
