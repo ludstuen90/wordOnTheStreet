c3d924c4d56bbca1705fa306f2cb068b732bc622
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2016-07-23 10:29:32 -0500

    initial commit

--- public/scripts/clientApp.js
+++ public/scripts/clientApp.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+var socket = io();
+var CHTAP = angular.module('CHTAP', []);
+
+CHTAP.controller('chatPage', ['$scope', '$http', function($scope, $http){
+$scope.hello = "it's me";
+
+$scope.chatMessages = [];
+
+$scope.sendChat = function(){
+  socket.emit('chat message', $scope.hello);
+  $scope.chatMessages.push($scope.hello);
+  $scope.hello = '';
+  return false;
+}; socket.on('chat message', function(msg){
+
+});
+
+}]);
